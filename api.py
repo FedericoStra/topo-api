a6cb4553c5ca182f91ca3528231331616260aa6f
Federico Stra <stra.federico@gmail.com>
2017-03-19 18:04:28 +0100

    Update schemas

--- api.py
+++ api.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from eve import Eve
 
 app = Eve()
--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 
 # Skip these if your db has no auth. But it really should.
-#MONGO_USERNAME = '<your username>'
-#MONGO_PASSWORD = '<your password>'
+# MONGO_USERNAME = '<your username>'
+# MONGO_PASSWORD = '<your password>'
 
 MONGO_DBNAME = 'topo-api'
 
@@ -20,59 +20,18 @@ RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 # individual items  (defaults to read-only item access).
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
-people_schema = {
-    # Schema definition, based on Cerberus grammar. Check the Cerberus project
-    # (https://github.com/pyeve/cerberus) for details.
-    'firstname': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 15,
-        'required': True,
-    },
-    'lastname': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 32,
-        'required': True,
-        # talk about hard constraints! For the purpose of the demo
-        # 'lastname' is an API entry-point, so we need it to be unique.
-        'unique': True,
-    },
-    # 'role' is a list, and can only contain values from 'allowed'.
-    'role': {
-        'type': 'list',
-        'allowed': ["author", "contributor", "copy"],
-    },
-    'position': {
-        'type': 'list',
-        'allowed': ["graduate student", "phd student", "post-doc", "researcher", "professor"],
-    },
-    # An embedded 'strongly-typed' dictionary.
-    'location': {
-        'type': 'dict',
-        'schema': {
-            'address': {'type': 'string'},
-            'city': {'type': 'string'}
-        },
-    },
-    'born': {
-        'type': 'datetime',
-    },
+from schemas import accounts_schema, people_schema
+
+accounts = {
+    'resource_methods': ['GET', 'POST'],
+    'item_methods': ['GET', 'PATCH', 'PUT', 'DELETE'],
+    'schema': accounts_schema
 }
 
 people = {
     # 'title' tag used in item links. Defaults to the resource title minus
     # the final, plural 's' (works fine in most cases but not for 'people')
     'item_title': 'person',
-
-    # by default the standard item entry point is defined as
-    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
-    # additional read-only entry point. This way consumers can also perform
-    # GET requests at '/people/<lastname>'.
-    'additional_lookup': {
-        'url': 'regex("[\w]+")',
-        'field': 'lastname'
-    },
 
     # We choose to override global cache-control directives for this resource.
     'cache_control': 'max-age=10,must-revalidate',
@@ -84,4 +43,8 @@ people = {
     'schema': people_schema
 }
 
-DOMAIN = {'users': {}, 'people': people, 'seminars': {}}
+DOMAIN = {
+    'accounts': accounts,
+    'people': people,
+    'seminars': {}
+}
